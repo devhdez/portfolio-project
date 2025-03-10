8ea766de9ae3d80e3f0060d910cd50801ef80d4a
devhdez <98553476+devhdez@users.noreply.github.com>
2025-03-10 22:29:04 +0000

    Fix and Feat CSS

--- src/Feat.jsx
+++ src/Feat.jsx
@@ -8,7 +8,7 @@ const Feat = () => {
                 <h2 className="Features-title"> Developer | Translator | SEO</h2>
                 <div className="Features-list">
                     <div className="Feature">
-                        <h3 className="Feature-title">Multilingual Websites</h3>
+                        <h3 className="Feature-title">Image Here</h3>
                         <p className="Feature-description">As a seasoned freelancer combining advanced web development and thoughtful translation/localisation services, I craft digital solutions that are both technically robust and culturally resonant. 
                             Whether you need a cutting-edge business website, an administrative application (think personnel selection systems), a dynamic webshop, a food delivery platform, or a CRM solution, I ensure that every digital asset is optimized for efficiency and tailored to the nuances of your target audience—even across different languages and cultures. 
                             In short, if it needs to appear on screen to boost your business's performance and engage a global customer base, I've got the expertise to make it shine both technically and linguistically.</p>
