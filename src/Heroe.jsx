881acdcf47ab11e9a0c69d347932f4c0c09fa967
devhdez <hrborja@gmail.com>
2025-03-12 20:23:44 +0000

    Adds StringSwitcher animation

--- src/Heroe.jsx
+++ src/Heroe.jsx
@@ -1,31 +1,17 @@
 import React, {useEffect} from 'react';
+import useStringSwitcher from './Stringswitcher';
+
 
 const Heroe = () => {
-    useEffect(() => {
-        const header = document.querySelector('.heroe-title');
-        const cursor = document.createElement('span');
-        cursor.className = 'blinking-cursor';
-        cursor.innerText = '|';
-        header.appendChild(cursor);
-    
-        return () => {
-            // Cleanup function: remove the cursor to avoid duplication on Strict Mode
-            if (header.contains(cursor)) {
-                header.removeChild(cursor);
-            }
-        };
-      }, []);
-
-    UseEffect(() => {
-        const h1 = document.querySelector('.heroe-title');
-        const text = h1.innerText;
-        
-
-    }, []);
+    const myString = useStringSwitcher('Expand your business', "Expande tu negocio", 5000);
 
     return (
         <section className="heroe">
-                <h1 className="heroe-title">Expand your business</h1>
+                {/* <h1 className="heroe-title">Expand your business</h1> */}
+                <h1 className="heroe-title">
+                    {myString}
+                <span className="blinking-cursor">|</span>
+                </h1>
                 <p>Let me convert your website into a multilingual one so you don't miss a single customer! <br/>
                 <br></br>
                 I can instantly make your business grow by exposing you to more clients and niche markets, all while keeping your website looking professional and easy to navigate.</p>
---
+++ src/Stringswitcher.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+function useStringSwitcher(initialString, targetString, interval) {
+  const [currentString, setCurrentString] = useState('');
+  const [toggle, setToggle] = useState(false);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const switcher = setInterval(() => {
+      setToggle((prevToggle) => !prevToggle);
+      setIndex(0); // Reset index when toggling
+      setCurrentString(''); // Clear the previous text when toggling
+    }, interval);
+
+    return () => clearInterval(switcher);
+  }, [interval]);
+
+  useEffect(() => {
+    const text = toggle ? targetString : initialString;
+
+    const typeEffect = setInterval(() => {
+      setCurrentString((prevString) => prevString + text.charAt(index));
+      setIndex((prevIndex) => prevIndex + 1);
+
+      if (index >= text.length - 1) {
+        clearInterval(typeEffect);
+      }
+    }, 100); // Typing interval
+
+    return () => clearInterval(typeEffect);
+  }, [index, toggle, initialString, targetString]);
+
+  return currentString;
+}
+
+export default useStringSwitcher;
